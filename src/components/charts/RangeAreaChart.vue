96b7ad6556d41c571910939855b951faa30a61cc
Lin-Rexter <aaaa35963596@gmail.com>
2023-11-21 00:45:41 +0800

    Add 客製化圖表-範圍面積圖

---
+++ src/components/charts/RangeAreaChart.vue
@@ -0,0 +1,43 @@
+<!-- Developed by Taipei Urban Intelligence Center 2023 -->
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps(["chart_config", "activeChart", "series"]);
+
+const chartOptions = ref({
+	chart: {
+		stacked: true,
+		toolbar: {
+			show: false,
+		},
+	},
+	colors: props.chart_config.color,
+	grid: {
+		show: false,
+	},
+	legend: {
+		show: props.chart_config.categories ? true : false,
+	},
+	markers: {
+		hover: {
+			sizeOffset: 5,
+		},
+	},
+	stroke: {
+		curve: "straight",
+	},
+});
+</script>
+
+<template>
+	<div v-if="activeChart === 'RangeAreaChart'">
+		<apexchart
+			width="100%"
+			height="350px"
+			type="rangeArea"
+			:options="chartOptions"
+			:series="series"
+		></apexchart>
+	</div>
+</template>
